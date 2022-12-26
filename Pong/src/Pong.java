27f9514bbd5557f7c2c11f5b70730bbddba28422
Tracey Young <tracey.y.ltp@gmail.com>
2022-12-27 11:10:23 +1300

    Created jPanel

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,24 +1,37 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 public class Pong extends JFrame {
 	
 	private final static String WINDOW_TITLE = "Pong";
 	
-	final static int WINDOW_WIDTH = 800;
-	final static int WINDOW_HEIGHT = 600;
+	private final static int WINDOW_WIDTH = 800;
+	private final static int WINDOW_HEIGHT = 600;
+	
+	
 	
 	public Pong() {
     	setTitle(WINDOW_TITLE);
 setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
     	setResizable(false);
+    	add(new PongPanel());
     	setVisible(true);
     	setDefaultCloseOperation(EXIT_ON_CLOSE);
 }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new Pong();
+	public static void main(String[] args) {      
+		 
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+ 
+          public void run() {
+ 
+                 new Pong();
+ 
+          }
+ 
+        });
+ 
+    }
 	
-	}
-
 }
